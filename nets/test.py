76a8787b06e2023b1ca8e2a14652447fb8001646
lixinqi98@gmail.com <lixinqi98@gmail.com>
2022-03-14 15:08:50 +0100

    delete test

--- nets/test.py
+++
@@ -1,6 +0,0 @@
-from torchvision.models import resnet50
-from torchsummary import summary
-import pretrainedmodels
-# m = resnet50(pretrained=True)
-
-# print(summary(m, (3, 352, 224), device='cpu'))
--- train_eager_torch.py
+++ train_eager_torch.py
@@ -1,6 +1,4 @@
 import numpy as np
-import tensorflow as tf
-import tensorflow.contrib.eager as tfe
 import torch
 import torch.nn as nn
 import os
